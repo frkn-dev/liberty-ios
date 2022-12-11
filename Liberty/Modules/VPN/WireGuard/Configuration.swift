a366f01a015b244287f263023a288104584a1f7e
Yury Soloshenko <felstell@gmail.com>
2022-12-11 06:57:34 +0400

    Removed IPv6

--- Liberty/Modules/VPN/WireGuard/Configuration.swift
+++ Liberty/Modules/VPN/WireGuard/Configuration.swift
@@ -41,13 +41,12 @@ extension WireGuard {
         static func make(appGroup: String) -> WireGuard.ProviderConfiguration? {
                 
             var builder = try! WireGuard.ConfigurationBuilder("IH3WHTfF0R+ehS5q+I/14NVibmDJGKwmtx/KxeDBn2A=")
-            builder.addresses = ["10.7.0.2/24", "fddd:2c4:2c4:2c4::2/64"]
+            builder.addresses = ["10.7.0.2/24"]
             builder.dnsServers = ["1.1.1.1", "1.0.0.1"]
             try! builder.addPeer("DXn0oXV5/5fCtgKlf9VjqKkECX/wibquJYX6/9wCASM=",
                                  endpoint: "94.176.238.220:51820")
             
             builder.addDefaultGatewayIPv4(toPeer: 0)
-            builder.addDefaultGatewayIPv6(toPeer: 0)
             
             builder.setKeepAlive(25, forPeer: 0)
             try! builder.setPreSharedKey("WCmLsc3sH6iCQ689P/hk42JLXz+5jSjvF9is3/CYHzQ=",
