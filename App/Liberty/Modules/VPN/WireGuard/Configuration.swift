a4fe94da5c9a129e60e44c4802833fee9349b40a
Yury Soloshenko <felstell@gmail.com>
2022-12-24 11:24:53 +0400

    Returned testConfiguration

--- App/Liberty/Modules/VPN/WireGuard/Configuration.swift
+++ App/Liberty/Modules/VPN/WireGuard/Configuration.swift
@@ -34,4 +34,28 @@ extension WireGuard {
                                                    configuration: config)
         }
     }
+    
+    struct TestConfiguration {
+        
+        static func make(appGroup: String) -> WireGuard.ProviderConfiguration? {
+                
+            var builder = try! WireGuard.ConfigurationBuilder("IH3WHTfF0R+ehS5q+I/14NVibmDJGKwmtx/KxeDBn2A=")
+            builder.addresses = ["10.7.0.2/24"]
+            builder.dnsServers = ["1.1.1.1", "1.0.0.1"]
+            try! builder.addPeer("DXn0oXV5/5fCtgKlf9VjqKkECX/wibquJYX6/9wCASM=",
+                                 endpoint: "94.176.238.220:51820")
+            
+            builder.addDefaultGatewayIPv4(toPeer: 0)
+            
+            builder.setKeepAlive(25, forPeer: 0)
+            try! builder.setPreSharedKey("WCmLsc3sH6iCQ689P/hk42JLXz+5jSjvF9is3/CYHzQ=",
+                                         ofPeer: 0)
+            
+            let config = builder.build()
+            
+            return WireGuard.ProviderConfiguration("fuckrkn1",
+                                                   appGroup: appGroup,
+                                                   configuration: config)
+        }
+    }
 }
