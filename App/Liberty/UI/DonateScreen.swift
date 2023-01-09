461d11df14c82627cd89d1cf47e463191c65ff8e
Yury Soloshenko <felstell@gmail.com>
2023-01-10 00:02:34 +0400

    Update donation links

--- App/Liberty/UI/DonateScreen.swift
+++ App/Liberty/UI/DonateScreen.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct DonateScreen: View {
     
     let supportItems = [
-        ("USDT(Tether)", "0xE92d1695483bd9E82A1AeEEa02E60797B055c53C"),
-        ("BTC", "bc1qe76609xu8qay4wt7tnvrxhx4spnjrywwh2jwf3"),
-        ("Ethereum", "0xE92d1695483bd9E82A1AeEEa02E60797B055c53C"),
-        ("NEAR", "2e1a065b84f27e3e8b7c9cd64200b7cf00ccd7b624da5996aba1185408ba5b5b"),
+        ("BTC", "bc1q5xjm7v20aee4tl2vngqju9k0uzajl2hvljqec6"),
+        ("Ethereum", "0x92A3Fad82F27938AA0D5c7509132DD781C4880bb"),
+        ("USDT(Eth)", "0x92A3Fad82F27938AA0D5c7509132DD781C4880bb"),
+        ("USDT(Tron)", "TKuMVvLHW9orkuVwKpC8AhTDUDQV91ZfeX"),
+        ("Solans", "3Crb3Kh6akz4n1qdMaG59e5jVSKR8VsZdPwF9GB8go44"),
         ("Credo (Georgian Card)","GE21CD0360000028302649"),
     ]
     
